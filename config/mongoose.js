d75a60d7f9ef28cf7c6de73def64c17a282545b0
rishabh <dsvvian.rishabh@gmail.com>
2018-10-11 23:19:17 +0530

    working

--- config/mongoose.js
+++ config/mongoose.js
@@ -6,7 +6,7 @@ const promisify = Promise.promisify;
 const log = require('./log')(module);
 const connections = require('./connections');
 
-mongoose.connection.openUri("mongodb://localhost/shop").then(() => {
+mongoose.connection.openUri("mongodb://Rishabh:rishabh123@ds261929.mlab.com:61929/chatapp").then(() => {
     log.info('Connected to DB!');
 }).catch((error) => {
     log.error(error);
